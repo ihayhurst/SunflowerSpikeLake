396a2393d880deafb5144e019da4b8f77a6b1d14
ihayhurst <29225030+ihayhurst@users.noreply.github.com>
2020-10-05 20:20:40 +0100

    hot code load file configured to  touch_to_reload

--- flask/app/views.py
+++ flask/app/views.py
@@ -9,7 +9,7 @@ def index():
     app_name = os.getenv("APP_NAME")
     flaskVer = flask.__version__
     if app_name:
-        message = f"Hello from {app_name}, a flask ver {flaskVer} app running in a Docker container behind Nginx!"
+        message = f"Hello from {app_name}, a flask ver {flaskVer} app running in a Docker container behind Nginx!: with redis and hot code load"
         return flask.render_template('index.html', message=message)
 
     return flask.render_template('index.html', message="Hello from Flask")
