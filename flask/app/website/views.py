e067eb47c9f279945390f39c2b2f65e97d39a908
ihayhurst <29225030+ihayhurst@users.noreply.github.com>
2020-10-07 22:53:07 +0100

    some debug code and environment fixing

--- flask/app/website/views.py
+++ flask/app/website/views.py
@@ -1,6 +1,6 @@
-#from app import app
 import os
-from flask import Flask, render_template, Blueprint, current_app
+from flask import render_template, Blueprint, current_app
+from flask import __version__ as __flask_version__
 website = Blueprint('website', __name__)
 
 
@@ -9,10 +9,10 @@ def website_home():
 
     # Use os.getenv("key") to get environment variables
     app_name = os.getenv("APP_NAME")
-    #flaskVer = flask.__version__
-    flaskVer = 66.6
+    print({k:v for k,v in current_app.config.items()})
+    flaskVer = __flask_version__
     if app_name:
-        message = f'Hello from {app_name}, a flask ver {flaskVer} app running in a Docker container behind Nginx!: with redis and hot code load'
+        message = f'Hello from {app_name}, a flask ver {flaskVer} app running in a Docker container behind Nginx! with redis and hot code load'
         return render_template('index.html', message=message)
 
     return render_template('index.html', message="Hello from Flask")
