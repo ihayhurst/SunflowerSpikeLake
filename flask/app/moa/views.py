00b21435241e48a875fda87931cf515f7b2c3e36
Ian Hayhurst <29225030+ihayhurst@users.noreply.github.com>
2020-10-14 22:23:47 +0100

    not pretty but talking to Oracle

--- flask/app/moa/views.py
+++ flask/app/moa/views.py
@@ -1,24 +1,39 @@
 from flask import  Blueprint
+from flask import current_app as app
 from flask_restful import Api, Resource
+from .. import db
 import cx_Oracle
 
 moa = Blueprint('moa', __name__)
 api = Api(moa)
 
-"""
-with cx_Oracle.connect(userName, password, "dbhost.example.com/orclpdb1",
-            encoding="UTF-8") as connection:
-    cursor = connection.cursor()
-    cursor.execute("insert into SomeTable values (:1, :2)",
-            (1, "Some string"))
-    connection.commit() 
-"""
-class TodoItem(Resource):
-    def get(self, id):
-        return {'task': 'Say "Hello, World!"'}
-
 @moa.route("/")
 def moa_home():
-    return "<h1>API Home</h1>"
+    dsn = cx_Oracle.makedsn(app.config['DATABASE_HOST'],
+                            app.config['DATABASE_PORT'],
+                            service_name="APEXRDP2")
+    id = 222
 
-api.add_resource(TodoItem, '/todos/<int:id>')
+    #conn_str = f"{app.config['DATABASE_USER']}, {app.config['DATABASE_PASSWORD']}, {dsn}"
+    conn_str ="SCO_MOA_ONTOLOGY/sc0_m04_0n70logy@//deawdoraappp001.c0teethmpf9v.eu-central-1.rds.amazonaws.com:1530/APEXRDP2"
+    print(conn_str)
+    sql = f"select * from MOA_ENTITY where entity_id={id}"
+    conn = cx_Oracle.connect(conn_str)
+    curr = conn.cursor()
+    curr.execute(sql)
+    data = curr.fetchall()
+    curr.close()
+    conn.close()
+    return f"return: {data}"
+    #return f"<h1>API Home</h1>{conn_str}"
+"""
+class Entity(Resource):
+    dsn = cx_Oracle.makedsn(app.config['DATABASE_HOST'],
+                            app.config['DATABASE_PORT'],
+                            app.config['DATABASE_NAME'])
+
+    conn_str = f"{app.config['DATABASE_USER']}, {app.config['DATABASE_PASSWORD']}, {dsn}, encoding='UTF-8'"
+    def get(self, id):
+
+api.add_resource(Entity, '/entity/<int:id>')
+"""
