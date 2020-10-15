99704c9d9da379e144aa046e4dcb5b979c056096
Ian Hayhurst <29225030+ihayhurst@users.noreply.github.com>
2020-10-15 17:26:37 +0100

    first endpoints working as JSON

--- flask/app/moa/views.py
+++ flask/app/moa/views.py
@@ -1,5 +1,6 @@
-from flask import  Blueprint
-from flask import current_app as app
+import json
+from flask import  Blueprint, Response
+#from flask import current_app as app
 from flask_restful import Api, Resource
 from .. import db
 import cx_Oracle
@@ -7,33 +8,48 @@ import cx_Oracle
 moa = Blueprint('moa', __name__)
 api = Api(moa)
 
-@moa.route("/")
-def moa_home():
-    dsn = cx_Oracle.makedsn(app.config['DATABASE_HOST'],
-                            app.config['DATABASE_PORT'],
-                            service_name="APEXRDP2")
-    id = 222
-
-    #conn_str = f"{app.config['DATABASE_USER']}, {app.config['DATABASE_PASSWORD']}, {dsn}"
+def dbGetConn():
     conn_str ="SCO_MOA_ONTOLOGY/sc0_m04_0n70logy@//deawdoraappp001.c0teethmpf9v.eu-central-1.rds.amazonaws.com:1530/APEXRDP2"
-    print(conn_str)
-    sql = f"select * from MOA_ENTITY where entity_id={id}"
     conn = cx_Oracle.connect(conn_str)
+    return conn
+
+def getData(sql):
+    conn = dbGetConn()
     curr = conn.cursor()
     curr.execute(sql)
     data = curr.fetchall()
+    jsonData = [dict(zip([key[0] for key in curr.description], row)) for row in data]
     curr.close()
     conn.close()
-    return f"return: {data}"
-    #return f"<h1>API Home</h1>{conn_str}"
-"""
-class Entity(Resource):
-    dsn = cx_Oracle.makedsn(app.config['DATABASE_HOST'],
-                            app.config['DATABASE_PORT'],
-                            app.config['DATABASE_NAME'])
+    return (jsonData if jsonData else None)
 
-    conn_str = f"{app.config['DATABASE_USER']}, {app.config['DATABASE_PASSWORD']}, {dsn}, encoding='UTF-8'"
+
+@moa.route('/')
+def moa_home():
+    id = 222
+
+    ##conn_str = f"{app.config['DATABASE_USER']}, {app.config['DATABASE_PASSWORD']}, {dsn}"
+    sql = f"select * from MOA_ENTITY where entity_id={id}"
+    data = getData(sql)
+    return f"return: {data}"
+
+@moa.route('/wibble')
+def wibble():
+    #data = moa.config'[DATABASE_HOST']
+    data = "some data"
+    return f"<h4>wibble: </h4>{data}"
+
+class Entity(Resource):
     def get(self, id):
+        sql = f"select * from MOA_ENTITY where entity_id={id}"
+        data  = getData(sql)
+        return f"{data}"
+
+class Entitys(Resource):
+    def get(self):
+        sql = f"select * from MOA_ENTITY"
+        data  = getData(sql)
+        return f"{data}"
 
 api.add_resource(Entity, '/entity/<int:id>')
-"""
+api.add_resource(Entitys, '/entity')
