9a7a0a3be6d5420526f03cb4fa64a2eaa303d69e
Ian Hayhurst <29225030+ihayhurst@users.noreply.github.com>
2020-10-16 10:33:12 +0100

    return json corectly (rather than as quoted string), render markdown of api doc on moa home

--- flask/app/moa/views.py
+++ flask/app/moa/views.py
@@ -1,14 +1,16 @@
-import json
 from flask import  Blueprint, Response
 #from flask import current_app as app
 from flask_restful import Api, Resource
-from .. import db
 import cx_Oracle
+import markdown
+import os
+from .. import db
 
 moa = Blueprint('moa', __name__)
 api = Api(moa)
 
 def dbGetConn():
+    ##conn_str = f"{app.config['DATABASE_USER']}, {app.config['DATABASE_PASSWORD']}, {dsn}"
     conn_str ="SCO_MOA_ONTOLOGY/sc0_m04_0n70logy@//deawdoraappp001.c0teethmpf9v.eu-central-1.rds.amazonaws.com:1530/APEXRDP2"
     conn = cx_Oracle.connect(conn_str)
     return conn
@@ -26,12 +28,17 @@ def getData(sql):
 
 @moa.route('/')
 def moa_home():
-    id = 222
+    """Present some documentation"""
 
-    ##conn_str = f"{app.config['DATABASE_USER']}, {app.config['DATABASE_PASSWORD']}, {dsn}"
-    sql = f"select * from MOA_ENTITY where entity_id={id}"
-    data = getData(sql)
-    return f"return: {data}"
+    # Open the README file
+    with open(os.path.join(moa.root_path) + '/README.md', 'r') as markdown_file:
+
+        # Read the content of the file
+        content = markdown_file.read()
+
+        # Convert to HTML
+        return markdown.markdown(content)
+
 
 @moa.route('/wibble')
 def wibble():
@@ -43,13 +50,13 @@ class Entity(Resource):
     def get(self, id):
         sql = f"select * from MOA_ENTITY where entity_id={id}"
         data  = getData(sql)
-        return f"{data}"
+        return data, 201
 
 class Entitys(Resource):
     def get(self):
         sql = f"select * from MOA_ENTITY"
         data  = getData(sql)
-        return f"{data}"
+        return data, 201
 
 api.add_resource(Entity, '/entity/<int:id>')
 api.add_resource(Entitys, '/entity')
