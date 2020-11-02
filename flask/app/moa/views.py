210ce95dd2aa73a10df06cc820a7307fb930b4ec
ihayhurst <29225030+ihayhurst@users.noreply.github.com>
2020-11-02 21:55:07 +0000

    linting

--- flask/app/moa/views.py
+++ flask/app/moa/views.py
@@ -4,20 +4,21 @@ from flask_restful import Api, Resource
 import cx_Oracle
 import markdown
 import os
-#from .. import db
 
-moa = Blueprint('moa', __name__)
+moa = Blueprint("moa", __name__)
 api = Api(moa)
 
+
 def dbGetConn():
-    dbhost = app.config['DATABASE_HOST']
-    dbport = app.config['DATABASE_PORT']
-    dbname = app.config['DATABASE_NAME']
-    dbuser = app.config['DATABASE_USER']
-    dbpw = app.config['DATABASE_PASSWORD']
+    dbhost = app.config["DATABASE_HOST"]
+    dbport = app.config["DATABASE_PORT"]
+    dbname = app.config["DATABASE_NAME"]
+    dbuser = app.config["DATABASE_USER"]
+    dbpw = app.config["DATABASE_PASSWORD"]
     conn_str = f"{dbuser}/{dbpw}@//{dbhost}:{dbport}/{dbname}"
     conn = cx_Oracle.connect(conn_str)
     return conn
+
 
 def getData(sql):
     conn = dbGetConn()
@@ -27,22 +28,21 @@ def getData(sql):
     jsonData = [dict(zip([key[0] for key in curr.description], row)) for row in data]
     curr.close()
     conn.close()
-    return (jsonData if jsonData else None)
+    return jsonData if jsonData else None
 
 
-@moa.route('/')
+@moa.route("/")
 def moa_home():
     """Present some documentation"""
 
     # Open the README file
-    with open(os.path.join(moa.root_path) + '/README.md', 'r') as markdown_file:
+    with open(os.path.join(moa.root_path) + "/README.md", "r") as markdown_file:
 
         # Read the content of the file
         content = markdown_file.read()
 
-
         # Convert to HTML
-        md = markdown.markdown(content, extensions=['tables', 'fenced_code'])
+        md = markdown.markdown(content, extensions=["tables", "fenced_code"])
         return md
 
 
@@ -52,12 +52,12 @@ class Entity(Resource):
             sql = f"select * from MOA_ENTITY where entity_id={id}"
         else:
             sql = f"select * from MOA_ENTITY"
-        data  = getData(sql)
+        data = getData(sql)
         return data, 201
 
 
 class Triple(Resource):
-    def get (self, tripleid=None, subjectid=None, predicateid=None,objectid=None):
+    def get(self, tripleid=None, subjectid=None, predicateid=None, objectid=None):
         if tripleid is not None:
             sql = f"select * from MOA_TRIPLE where triple_id={tripleid}"
         elif subjectid is not None:
@@ -69,14 +69,14 @@ class Triple(Resource):
         else:
             sql = f"select * from MOA_TRIPLE"
         data = getData(sql)
-        return data,201
+        return data, 201
 
-api.add_resource(Entity, '/entity/<int:id>', endpoint='entity')
-api.add_resource(Entity, '/entity', endpoint='entitys')
-api.add_resource(Triple, '/triple/triple/<int:tripleid>', endpoint='tripleid')
-api.add_resource(Triple, '/triple/subject/<int:subjectid>', endpoint='subjectid')
-api.add_resource(Triple, '/triple/predicate/<int:predicateid>', endpoint='predicateid')
-api.add_resource(Triple, '/triple/object/<int:objectid>', endpoint='objectid')
-api.add_resource(Triple, '/triple/<int:tripleid>', endpoint='triple')
-api.add_resource(Triple, '/triple', endpoint='triples')
 
+api.add_resource(Entity, "/entity/<int:id>", endpoint="entity")
+api.add_resource(Entity, "/entity", endpoint="entitys")
+api.add_resource(Triple, "/triple/triple/<int:tripleid>", endpoint="tripleid")
+api.add_resource(Triple, "/triple/subject/<int:subjectid>", endpoint="subjectid")
+api.add_resource(Triple, "/triple/predicate/<int:predicateid>", endpoint="predicateid")
+api.add_resource(Triple, "/triple/object/<int:objectid>", endpoint="objectid")
+api.add_resource(Triple, "/triple/<int:tripleid>", endpoint="triple")
+api.add_resource(Triple, "/triple", endpoint="triples")
--- flask/app/website/views.py
+++ flask/app/website/views.py
@@ -1,25 +1,30 @@
 import os
 from flask import render_template, Blueprint, current_app
 from flask import __version__ as __flask_version__
-website = Blueprint('website', __name__,
-                     static_folder='static',
-                     static_url_path='/website/static',
-                     template_folder='templates')
+
+website = Blueprint(
+    "website",
+    __name__,
+    static_folder="static",
+    static_url_path="/website/static",
+    template_folder="templates",
+)
 
 
 @website.route("/")
 def website_home():
-    return render_template('index.html', message='heloo', )
+    return render_template("index.html", message="heloo",)
+
 
 @website.route("about/")
 def website_about():
 
     # Use os.getenv("key") to get environment variables
     app_name = os.getenv("APP_NAME")
-    envprint = {k:v for k,v in current_app.config.items()}
+    envprint = {k: v for k, v in current_app.config.items()}
     flaskVer = __flask_version__
     if app_name:
-        message = f'Hello from {app_name}, a flask ver {flaskVer} app running in a Docker container behind Nginx! with redis and hot code load'
-        return render_template('about.html', message=message, envprint=envprint)
+        message = f"Hello from {app_name}, a flask ver {flaskVer} app running in a Docker container behind Nginx! with redis and hot code load"
+        return render_template("about.html", message=message, envprint=envprint)
 
-    return render_template('about.html', message="Hello from Flask", envprint=envprint)
+    return render_template("about.html", message="Hello from Flask", envprint=envprint)
